a8a10bf6ff8ddcdac85e9135eb25691fec8b914a
Alisher Kadraliev <alisherkadraliev@gmail.com>
2024-07-15 20:38:42 +0300

    login and register done

--- auth.ts
+++ auth.ts
@@ -2,10 +2,32 @@ import NextAuth from "next-auth"
 import authConfig from "./auth.config"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import { db } from "./lib/db"
+import { getUserById } from "./data/user"
+
+
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
+    callbacks: {
+        async session({ token, session }) {
+            if (token.role && session.user) {
+                session.user.role = token.role as "ADMIN" | "USER"
+            }
+            if (token.sub && session.user) {
+                session.user.id = token.sub
+            }
+            return session
+        },
+        async jwt({ token }) {
+            if (!token.sub) return token
+            const existingUser = await getUserById(token.sub)
+            if (!existingUser) return token
+            token.role = existingUser.role
+            return token
+        }
+    },
     adapter: PrismaAdapter(db),
     session: { strategy: "jwt" },
-    
+
 
     ...authConfig
 })
--- components/auth/social.tsx
+++ components/auth/social.tsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import { FcGoogle } from 'react-icons/fc'
 import { FaFacebook } from "react-icons/fa";
-
+import { signIn } from 'next-auth/react';
 import { Button } from '../ui/button'
+import { DEFAULT_LOGIN_REDIRECT } from '@/routes';
 const Social = () => {
+    const onClick = (provider: "google") => {
+        signIn(provider, {
+            callbackUrl: DEFAULT_LOGIN_REDIRECT,
+        })
+    }
+
     return (
         <div className='flex items-center w-full gap-x-2'>
-            <Button className='w-full' size="lg" variant={'outline'} >
+            <Button onClick={() => onClick('google')} className='w-full' size="lg" variant={'outline'} >
                 <FcGoogle className='w-7 h-7' />
             </Button>
             <Button className='w-full' size="lg" variant={'outline'} >
