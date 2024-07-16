acdf3770b731870adc184323117b02d41e53c5f0
Alisher Kadraliev <alisherkadraliev@gmail.com>
2024-07-16 17:56:30 +0300

    zustang added

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -1,15 +1,11 @@
 import { Navbar } from "./_components/navbar"
+import AdminPanelLayout from "@/components/admin-panel/admin-panel-layout";
 
 interface ProtectedLayout {
     children: React.ReactNode
 }
 
 const ProtectedLayout = ({ children }: ProtectedLayout) => {
-    return (
-        <div>
-            <Navbar />
-            {children}
-        </div>
-    )
+    return <AdminPanelLayout>{children}</AdminPanelLayout>;
 }
 export default ProtectedLayout
